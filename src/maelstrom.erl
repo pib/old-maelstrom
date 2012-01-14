a15849d53a00176210fbe92151d7ccbea591fe3c
ixmatus <devnull@localhost>
2012-01-13 17:28:04 -0800

    Handling cases in which a node goes down more gracefully

--- src/maelstrom.erl
+++ src/maelstrom.erl
@@ -40,7 +40,7 @@ map(_Fun, [], Length, _Limit, _Pos, Acc) when Length == length(Acc) ->
     Acc.
 
 %% Map over a list on multiple nodes
-map(Fun, List, {Split, NodeSpec}) ->
+map(Fun, List, {Split, NodeSpec}) when length(List) >= Split ->
     application:start(lager),
     
     %% Get a list of good nodes we can send work to
@@ -55,9 +55,15 @@ map(Fun, List, {Split, [Node|T]}, Length, Limit, Pos, Acc, NodAcc) when Limit > 
     %% Get a worker
     Worker = ml_server:checkout(),
     
-    {Chunk, Rest} = lists:split(Split, List),
+    {Chunk, Rest} = case length(List) > 1 of
+        true ->
+            lists:split(Split, List);
+        _Else ->
+            [H|_T] = List,
+            {[H], []}
+    end,
     
-    ml_worker:work(Worker, self(), {fun(N,F,A)-> rpc:call(N, maelstrom, map, [F, A]) end, [Node, Fun, Chunk]}),
+    ml_worker:work(Worker, self(), {fun(N,F,A,P)-> rpc(N,F,A,P) end, [Node, Fun, Chunk, self()]}),
     map(Fun, Rest, {Split, T}, Length, Limit, Pos + 1, Acc, lists:append(NodAcc, [Node]));
 
 %% If limit (length of nodespec) is equal to Pos (number of nodes assigned)
@@ -65,7 +71,19 @@ map(Fun, [H|T], {Split, Nodes}, Length, Limit, Pos, Acc, NodAcc) when Limit == P
     receive
         {done, Payload} ->
             [N1|Rest] = NodAcc,
-            map(Fun, [H|T], {Split, lists:append(Nodes, [N1])}, Length, Limit, Pos - 1, Payload++Acc, Rest)
+            map(Fun, [H|T], {Split, lists:append(Nodes, [N1])}, Length, Limit, Pos - 1, Payload++Acc, Rest);
+        
+        %% This is coming from the rpc/4 function so we can reclaim data
+        {rpcnodedown, Node, Data} ->
+            %% Put the data (it should be a list) back into the list
+            Reunited = lists:append(T, Data),
+            lager:info("Node ~p crashed", [Node]),
+            
+            %% Don't persist the dead node!
+            D = lists:delete(Node, NodAcc),
+            
+            %% Pos must not be decremented because we still may need to receive a result from the other node!
+            map(Fun, [H|Reunited], {Split, Nodes}, Length, Limit, Pos, Acc, D)
     after
         20000 ->
             [N1|Rest] = NodAcc,
@@ -76,7 +94,17 @@ map(Fun, [H|T], {Split, Nodes}, Length, Limit, Pos, Acc, NodAcc) when Limit == P
 map(Fun, [], Spec, Length, Limit, Pos, Acc, NodAcc) when length(Acc) < Length ->
     receive
         {done, Payload} ->
-            map(Fun, [], Spec, Length, Limit, Pos, Payload++Acc, NodAcc)
+            map(Fun, [], Spec, Length, Limit, Pos, Payload++Acc, NodAcc);
+        
+        %% This is coming from the rpc/4 function so we can reclaim data
+        {rpcnodedown, Node, Data} ->
+            lager:info("Node ~p crashed", [Node]),
+            
+            %% Don't persist the dead node!
+            D = lists:delete(Node, NodAcc),
+            
+            %% Pos must not be decremented because we still may need to receive a result from the other node!
+            map(Fun, Data, Spec, Length, Limit, Pos, Acc, D)
     after
         20000 ->
             map(Fun, [], Spec, Length, Limit, Pos, [none|Acc], NodAcc)
@@ -86,12 +114,25 @@ map(Fun, [], Spec, Length, Limit, Pos, Acc, NodAcc) when length(Acc) < Length ->
 map(_Fun, [], _Spec, Length, _Limit, _Pos, Acc, _NodAcc) when Length == length(Acc) ->
     Acc.
 
+%% Make RPC call - if node is down when call is made, exit so we intentionally crash the worker
+rpc(Node, Fun, Args, Parent) ->
+    Res = rpc:call(Node, maelstrom, map, [Fun, Args]),
+    case Res of
+        {badrpc, Reason} ->
+            %% Crash the worker intentionally, work will be lost
+            Parent ! {rpcnodedown, Node, Args},
+            exit(Reason);
+        _Else ->
+            Res
+    end.
+
 %% Check the given nodes and return a list of those that responded
 check_nodes(NodeList) ->
     check_nodes(NodeList, []).
 check_nodes([H|T], Acc) ->
     case net_adm:ping(H) of
         pong ->
+            
             check_nodes(T, lists:append(Acc, [H]));
         pang ->
             lager:info("~p is not responding...", [H]),
